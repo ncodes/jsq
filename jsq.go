a71693ec980f22f5ac9dc3669195581b00958e52
Kennedy Idialu <kennedyidialu@gmail.com>
2017-05-25 09:57:53 +0100

    Added JSQ query interface in jsq.go

--- jsq.go
+++ jsq.go
@@ -15,6 +15,22 @@ import (
 	"github.com/jinzhu/inflection"
 )
 
+// Query defines an interface for JSQL query implementations
+type Query interface {
+
+	// Parse builds the query
+	Parse(jsonQuery string) error
+
+	// Set the table to query
+	SetTable(table interface{}, plural bool)
+
+	// Find the records matching the prepared query
+	Find(out interface{}, op ...QueryOption) error
+
+	// Count counts the number of records matching the prepared query
+	Count() (int64, error)
+}
+
 var (
 	// ErrNotFound indicates a missing data
 	ErrNotFound = fmt.Errorf("not found")
